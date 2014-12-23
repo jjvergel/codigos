6fd3d5cf7187ee0608c85fe818eb6448b103ec7e
jjvergel <juanjose.vergel@gmail.comgit config --global user.email juanjose.vergel@gmail.com>
2014-12-22 20:03:18 -0500

    renovacion de repositorio por errores

--- rcomandos.R
+++ rcomandos.R
@@ -2,18 +2,59 @@
 dir()
 getwd()
 setwd("../")
-setwd("./speedcamera")
+setwd("./3gcd")
+getwd()
 
-dir.create("speedcamera")
+dir.create("talcosa")
 download.file(fileurl, destfile = "./speedcamera/cameras.csv")
 
 data <- read.csv("camera.csv")
 read.table("./speedcamera/camera.csv", sep=",", header=TRUE)
 
-## INSTALAR PAQUETES
-install.packages("XML")
-library(XML)
+??read
+## INSTALAR PAQUETES WINDOWS
+install.packages("swirl")
+library(swirl)
+swirl()
+## INSTALAR PAQUETES UBUNTU....lineas para el TERMINAL
+sudo apt-get install libxml2-dev ## XML
+sudo apt-get install r-cran-testthat ## testthat
+
+packageStatus()
 
 ## SET NEW...PONER LIBRERIA CUANDO NO DEJA INSTALAR UN PAQUETE...WINDOWS
-.libPaths("/users/juanjos?/documents/r/win-library/3.1")
 .libPaths()
+.libPaths("/users/juanjos?/documents/r/win-library/3.1")
+
+## COMANDOS PARA DATAFRAMES
+dim (data)
+summary(data[3])
+names(data)
+head(data)
+head(data[37], 10)
+
+subset(data, data[,2]==state)
+x <- data.frame(c1, c2)
+class(data)
+as.numeric(data)
+as.character(data)
+as.logical(data)
+table(data)
+
+cbind(x,y)
+rbind(x,y)
+
+list()  ## lista de diferrentes tipos de objetos... ver proyecto
+unclass(data) ## convierte niveles en enteros
+
+ncol (data) ## numero de columnas de un data frame
+nrow (data)
+
+## SWIRL comandos
+skip()
+play()
+nxt()
+bye()
+main()
+info()
+
